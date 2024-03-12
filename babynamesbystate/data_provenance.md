80a312b478313c5ee971b8eb39a3478ac23ab34c
Julien Brun <brunj7@users.noreply.github.com>
2024-03-12 13:13:57 -0700

    move babynames to data

--- babynamesbystate/data_provenance.md
+++
@@ -1,3 +0,0 @@
-Source: US Social Security, https://www.ssa.gov/oact/babynames/limits.html
-
-Downloaded on: 2023-08-16
--- babynamesbystate/rename.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# renaming baby files of 2022
-# removing the leading "STATE." from the name
-for file in *.TXT; do
-    echo $file
-    mv $file ${file/STATE\./}
-done
